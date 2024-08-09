d3767e2866fbe0a637d136dc5dd563a323700b1e
namnthe140786 <namnthe140786@fpt.edu.vn>
2024-08-09 10:50:03 +0700

    fix_product_namnth

--- SWP391.E.BL5-G1/src/java/dal/productDAO.java
+++ SWP391.E.BL5-G1/src/java/dal/productDAO.java
@@ -25,205 +25,7 @@ public class productDAO extends DBContext {
     ResultSet rs = null;
 
     //namnthe140786
-    public List<Product> getProduct() {
-        List<Product> list = new ArrayList<>();
-        String sql = """
-                     select c.category_name ,  p.product_id , p.product_name, p.product_price, p.product_describe, p.quantity,p.img, p.category_id from  
-                     product p inner join category c on p.category_id = c.category_id""";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                Category c = new Category(rs.getInt(8), rs.getString(1));
-                list.add(new Product(c, rs.getString(2), rs.getString(3), rs.getFloat(4), rs.getString(5), rs.getInt(6), rs.getString(7)));
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return list;
-    }
-     public List<Color> getColor() {
-        List<Color> list = new ArrayList<>();
-        String sql = "select * from product_color";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                list.add(new Color(rs.getString(1), rs.getString(2)));
-            }
-        } catch (Exception e) {
-        }
-        return list;
-    }
-     public List<Size> getSize() {
-        List<Size> list = new ArrayList<>();
-        String sql = "select * from product_size";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                list.add(new Size(rs.getString(1), rs.getString(2)));
-            }
-        } catch (Exception e) {
-        }
-        return list;
-    }
-     public List<Product_Active> getActive(){
-        List<Product_Active> list = new ArrayList<>();
-         String sql = "select * from product_active";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                list.add(new Product_Active(rs.getString(1), rs.getString(2)));
-            }
-        } catch (Exception e) {
-        }
-        return list;
-    }
-     public Category getCategoryByName(String name) {
-        String sql = "select * from Category where category_name = ?";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql);
-            ps.setString(1, name);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                return new Category(rs.getInt(1), rs.getString(2));
-            }
-        } catch (Exception e) {
-        }
-        return null;
-    }
-     public void insertCategory(String name) {
-        String sql = " insert into Category (category_name) values(?)";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql);
-            ps.setString(1, name);
-            ps.executeUpdate();
-        } catch (Exception e) {
-        }
-    }
-     public void ProductDelete(String product_id) {
-        String sql = "delete from product_size where product_id=?";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql);
-            ps.setString(1, product_id);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        String sql1 = "delete from product_color where product_id=?";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql1);
-            ps.setString(1, product_id);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        String sql2 = "delete from product_img where product_id=?";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql2);
-            ps.setString(1, product_id);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        String sq3 = "delete from product where product_id=?";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sq3);
-            ps.setString(1, product_id);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        String sql4 ="delete from product_active where product_id=?";
-             try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql4);
-            ps.setString(1, product_id);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-    }
-     public void updateProduct(Product product, int cid, List<Color> listColor, List<Size> listSize) {
-        try {
-            // Update product details
-            String sql = "UPDATE product SET category_id=?, product_name=?, product_price=?, product_describe=?, quantity=?" + (!product.getImg().equalsIgnoreCase("images/") ? ", img=?" : "") + " WHERE product_id=?";
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(sql);
-            ps.setInt(1, cid);
-            ps.setString(2, product.getProduct_name());
-            ps.setFloat(3, product.getProduct_price());
-            ps.setString(4, product.getProduct_describe());
-            ps.setInt(5, product.getQuantity());
-            if (!product.getImg().isEmpty()) {
-                ps.setString(6, product.getImg());
-            }
-            ps.setString(!product.getImg().equalsIgnoreCase("images/") ? 7 : 6, product.getProduct_id());
-            ps.executeUpdate();
-
-            // Delete existing sizes and colors and actives
-            String deleteSizesSQL = "DELETE FROM product_size WHERE product_id=?";
-            ps = conn.prepareStatement(deleteSizesSQL);
-            ps.setString(1, product.getProduct_id());
-            ps.executeUpdate();
-
-            String deleteColorsSQL = "DELETE FROM product_color WHERE product_id=?";
-            ps = conn.prepareStatement(deleteColorsSQL);
-            ps.setString(1, product.getProduct_id());
-            ps.executeUpdate();
-             String deleteActiveSQL = "DELETE FROM product_active WHERE product_id=?";
-            ps = conn.prepareStatement(deleteActiveSQL);
-            ps.setString(1, product.getProduct_id());
-            ps.executeUpdate();
-            // Insert new sizes
-            String insertSizeSQL = "INSERT INTO product_size (product_id, size) VALUES (?, ?)";
-            for (Size size : listSize) {
-                ps = conn.prepareStatement(insertSizeSQL);
-                ps.setString(1, product.getProduct_id());
-                ps.setString(2, size.getSize());
-                ps.executeUpdate();
-            }
-            //Insert new Active
-                String insertActiveSQL = "INSERT INTO product_active (product_id, active) VALUES (?, ?)";
-                ps = conn.prepareStatement(insertActiveSQL);
-                ps.setString(1, product.getProduct_id());
-                ps.setString(2, product.getActive().getAcitve());
-                ps.executeUpdate();
-            
-
-            // Insert new colors
-            String insertColorSQL = "INSERT INTO product_color (product_id, color) VALUES (?, ?)";
-            for (Color color : listColor) {
-                ps = conn.prepareStatement(insertColorSQL);
-                ps.setString(1, product.getProduct_id());
-                ps.setString(2, color.getColor());
-                ps.executeUpdate();
-            }
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-     
-    ////
-    public List<Category> getCategory() {
+      public List<Category> getCategory() {
         List<Category> list = new ArrayList<>();
         String sql = "select * from category";
         try {
@@ -237,7 +39,6 @@ public class productDAO extends DBContext {
         }
         return list;
     }
-    
     public void insertProduct(Product product) {
         String sql = "insert into product (product_id,category_id,product_name,product_price,product_describe,quantity,img) values(?,?,?,?,?,?,?)";
         try {
@@ -343,16 +144,12 @@ public class productDAO extends DBContext {
     public static void main(String[] args) {
         productDAO dao = new productDAO();
 
-        List<Product> top10Products = dao.getTop10Product();
+        List<Category> top10Products = dao.getCategory();
 
         // Print the details of each top 10 product
-        for (Product product : top10Products) {
-            System.out.println("Product ID: " + product.getProduct_id());
-            System.out.println("Product Name: " + product.getProduct_name());
-            System.out.println("Product Price: " + product.getProduct_price());
-            System.out.println("Product Description: " + product.getProduct_describe());
-            System.out.println("Quantity: " + product.getQuantity());
-            System.out.println("Image: " + product.getImg());
+        for (Category product : top10Products) {
+            System.out.println("Product ID: " + product.getCategory_id());
+            System.out.println("Product Name: " + product.getCategory_name());
             System.out.println("----------------------------------");
         }
     }
