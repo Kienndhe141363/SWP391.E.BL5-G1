0304740bd7d774dc914a476208b356b63d607971
dungnthe130159 <dungnthe130159@fpt.edu.vn>
2024-08-17 19:58:42 +0700

    change and update

--- SWP391.E.BL5-G1/src/java/admin/ProductManager.java
+++ SWP391.E.BL5-G1/src/java/admin/ProductManager.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Controller.Admin;
+package admin;
 
 import dal.productDAO;
 import model.Category;
