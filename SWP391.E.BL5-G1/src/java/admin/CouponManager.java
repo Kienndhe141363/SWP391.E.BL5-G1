975b14e160798299c35664338c662280783a09b5
Thang <91139472+putaa1@users.noreply.github.com>
2024-08-21 12:48:37 +0700

    Fix session

--- SWP391.E.BL5-G1/src/java/admin/CouponManager.java
+++ SWP391.E.BL5-G1/src/java/admin/CouponManager.java
@@ -79,7 +79,7 @@ public class CouponManager extends HttpServlet {
         }
 
         model.User user = (model.User) session.getAttribute("user");
-        if (!user.getIsStoreStaff().equals("1")) {
+        if (!user.getIsStoreStaff().equals("true")) {
             response.sendRedirect("home");
             return;
         }
