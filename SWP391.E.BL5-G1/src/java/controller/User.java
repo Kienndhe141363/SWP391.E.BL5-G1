032803610ae24a640c562c3e1e0dc73993b9776a
kienndhe141363 <kienndhe141363@fpt.edu.vn>
2024-08-22 02:11:24 +0700

    update sendmail

--- SWP391.E.BL5-G1/src/java/controller/User.java
+++ SWP391.E.BL5-G1/src/java/controller/User.java
@@ -19,6 +19,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import model.BillDetail;
 import model.UserC;
 
 /**
@@ -104,7 +105,14 @@ public class User extends HttpServlet {
                 response.sendRedirect("user?action=myaccount");
             }
         }
-
+        if (action.equals("showdetail")) {
+            String bill_id = request.getParameter("bill_id");
+            int id = Integer.parseInt(bill_id);
+            billDAO dao = new billDAO();
+            List<BillDetail> detail = dao.getDetail(id);
+            request.setAttribute("detail", detail);
+            request.getRequestDispatcher("billdetail.jsp").forward(request, response);
+        }
         if (action.equals("updateinfo")) {
             HttpSession session = request.getSession();
             model.User user = (model.User) session.getAttribute("user");
